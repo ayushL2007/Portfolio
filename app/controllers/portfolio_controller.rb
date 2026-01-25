b441c20820e7b38cad17dd73079967a0bd2dc82e
ayushL2007 <ayushlahiri2008@gmail.com>
2026-01-25 10:17:14 +0000

    Projects Catalogue ✅

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -2,38 +2,15 @@ require 'octokit'
 
 class PortfolioController < ApplicationController
     def main
-        fetch_repos
-        @projects = @repos
-    end
+        client = RepoDetailsService.new
+        data = client.fetch_data
 
-    private
-    def fetch_repos
-        # In your Controller or Service Object
-        client = Octokit::Client.new(access_token: @token)
+        p data
 
-        # Enable auto-pagination to fetch more than 30 repos
-        client.auto_paginate = true
-
-        # Fetch repositories for a specific user
-        @repos = client.repos("ayushL2007")
-
-        p @repos.last.full_name
-        @languages = @repos.map {|r| client.languages(r.full_name)}
-        p @languages
-        trim_description
+        @projects = data["projects"]
+        @languages = data["languages"]
+        @best_repos = data["best_repos"]
     end
 
 
-
-    private
-    def trim_description
-        @repos.each do |repo|
-            desc = repo.description
-            if desc.split.length > 20
-                desc = desc.split "."
-                desc = desc[0..desc.length-2].join ". "  
-            end
-            repo.description = desc
-        end
-    end
 end
